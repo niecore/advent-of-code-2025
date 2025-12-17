99b1892cebb115204d53274aa6d50fe6ecc984bd
Nico Müller <nico.mueller@de.bosch.com>
2025-12-17 23:04:18 +0100

    day 06 part 2

--- src/day06.erl
+++ src/day06.erl
@@ -22,7 +22,39 @@ part1() ->
     lists:sum(Results).
 
 part2() ->
-    ok.
+    Input = input:read_input(6),
+    %% I split here Ops and Numbers because there
+    %% was a problem with my transpose function
+    %% because my input gets trimmed by the input
+    %% module. I dont want to change the input
+    %% for previous parts now.
+    [OpsTokens | _ ] = lists:reverse(Input),
+    Ops = string:tokens(OpsTokens, " "),
+
+    %% the only difference in the transpose is that
+    %% we transpose on single characters
+    %% and not on tokens.
+    Numbers = transpose(lists:droplast(Input)),
+
+    %% Created nested lists based on empty columns
+    Blocks = lists:foldr(fun(X, [H | T]) ->
+        case string:trim(X) =:= "" of
+            true -> [[], H | T];
+            false -> [[X | H] | T]
+        end
+    end, [[]], Numbers),
+    %% Trim strings and parse integers of nested lists
+    BlocksIntegers = [[list_to_integer(string:trim(S)) || S <- Block] || Block <- Blocks],
+    %% Readd Ops to nested lists
+    BlocksWithOps = lists:zip(Ops, BlocksIntegers),
+
+    Results = [case Operator of
+        "+" -> lists:sum(X);
+        "*" -> lists:foldl(fun(A, B) -> A * B end, 1, X);
+        _ -> 0
+    end || {Operator, X} <- BlocksWithOps],
+    lists:sum(Results).
+
 
 transpose([[] | _]) -> [];
 transpose(Rows) ->
